c07f1ff09169263ac21e8881e46398d977ae4361
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-03-29 23:03:22 +0300

    0.3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-
+const https = require("https");
 //--------------------------------------------------------
 
 const app = express(); //uygulama sabitim
@@ -20,11 +20,57 @@ app.get("/", function (req, res) {
 //--------------------------------------------------------
 
 app.post("/", function (req, res) {
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
+  const firstName = req.body.fName;
 
-  console.log(firstName, lastName, email);
+  const lastName = req.body.lName;
+
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  //https://mailchimp.com/
+  //bu adresten kayıt ol ve api keyini al
+
+  const url = "https://usX.api.mailchimp.com/3.0/lists/"; // usX = api keyinde us yazan yerin yaninda olan karakteri buraya yaz. en son /yanina görünür kimliği yaz
+
+  const options = {
+    method: "POST",
+    auth: "NOLDOR:", //Noldor:Api keyi lazım
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+
+  request.end();
+});
+
+//--------------------------------------------------------
+
+app.post("/failure.html", function (req, res) {
+  res.redirect("/");
 });
 
 //--------------------------------------------------------
